b72fc4a08004258b672351e9ffab758ff0c19151
Pac4games <amanoskull42@gmail.com>
2022-10-18 17:01:20 +0100

    Added .gitignore and ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,21 +6,48 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 23:21:16 by paugonca          #+#    #+#             */
-/*   Updated: 2022/10/17 23:29:37 by paugonca         ###   ########.fr       */
+/*   Updated: 2022/10/18 16:59:44 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+char	*convert(size_t p, long num, int aux, char *res)
+{
+	p--;
+	while (num > 0)
+	{
+		res[p + aux] = num % 10 + 48;
+		num /= 10;
+		p--;
+	}
+	res[p] = '\0';
+	return (res);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*res;
-	int		p;
+	long	num;
+	size_t	p;
+	int		aux;
 
 	p = 0;
-	if (n < 0)
+	aux = 0;
+	num = (long)n;
+	while (n)
 	{
-		res[p] = '-';
+		n /= 10;
 		p++;
 	}
+	res = (char *)malloc(p + 1);
+	if (!res)
+		return (0);
+	if (num < 0)
+	{
+		res[0] = '-';
+		num *= -1;
+		aux++;
+	}
+	return (convert(p, num, aux, res));
 }
